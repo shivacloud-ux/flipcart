675fcb3b326a43a4cbc0f2994c90fb8ac1f65345
Shiva <shivatms168@gmail.com>
2026-05-23 13:03:19 -0700

    add two files

---
+++ src/main/java/com/example/flipcart/visa.java
@@ -0,0 +1,14 @@
+package com.example.flipcart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class visa
+{
+    @GetMapping("/visa")
+    public String getData()
+    {
+        return "your order has been shipped successfully";
+    }
+}
