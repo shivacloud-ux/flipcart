a8641dc9a8a83db93526a139c8d4c0e9c38ebb7d
Shiva <shivatms168@gmail.com>
2026-05-24 03:31:31 -0700

    all changes have been updated

--- src/main/java/com/example/flipcart/shipping.java
+++ src/main/java/com/example/flipcart/shipping.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class shipping
-{
+public class shipping {
     @GetMapping("/shipping")
-    public String getData()
-    {
+    public String getData() {
         return "your order has been shipped successfully";
+    }
+
+    @GetMapping("/tracking")
+    public String trackOrder() {
+        return "Your order is in transit and will be delivered soon";
+    }
+
+    @GetMapping("/status")
+    public String orderStatus() {
+        return "Your order status is: Delivered";
     }
 }
